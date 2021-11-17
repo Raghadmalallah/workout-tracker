af13ffea64be531945948007df4597b9cd7d4adc
Raghad Malallah <raghadmalallah@gmail.com>
2021-11-16 21:29:55 -0800

    server.js

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const compression = require('compression')
 
 const PORT = process.env.PORT || 3000;
 
-const app = express()
- 
-// compress all responses
-app.use(compression())
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  );
 
-// routes
 app.use(require("./routes/api.js"));
 
+
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
