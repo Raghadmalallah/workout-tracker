68f98d810ca941797b385c69cb3f95818fa4e199
Raghad Malallah <raghadmalallah@gmail.com>
2021-11-16 22:04:54 -0800

    adding html rout

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(logger("dev")); 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +23,7 @@ mongoose.connect(
   );
 
 app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"))
 
 
 app.listen(PORT, () => {
